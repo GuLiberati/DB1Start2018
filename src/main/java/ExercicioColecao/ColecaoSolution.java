31b099bffa27fed75aa63fa5e430e9f52965ec6a
GuLiberati <gustavolibersti@gmail.com>
2018-08-18 10:26:58 -0300

    Exercicio Estados INCOMPLETO

--- src/main/java/ExercicioColecao/ColecaoSolution.java
+++ src/main/java/ExercicioColecao/ColecaoSolution.java
@@ -1,80 +1,168 @@
 package ExercicioColecao;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
-
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ColecaoSolution {
 
-	public static void main(String [] args) {
-		
-		exercicio1(); 
+	public static void main(String[] args) {
+
+		exercicio1();
 		exercicio2();
 		exercicio3();
-		
+		exercicio4();
+		exercicio5();
+		exercicio6();
+		exercicio7();
+		exercicio8();
+		exercicio14();
 	}
-	
+
 	public static List<String> exercicio1() {
-	
-			List<String> nomesCores = new ArrayList<String>();
-			nomesCores.add("Vermelho");
-			nomesCores.add("Preto");
-			nomesCores.add("Azul");
-			nomesCores.add("Roxo");
-			nomesCores.add("Amarelo");
-			nomesCores.add("Rosa");
-			nomesCores.add("Verde");
-			nomesCores.add("Branco");
-			
-			return nomesCores;
-		
+
+		List<String> nomesCores = new ArrayList<String>();
+		nomesCores.add("Vermelho");
+		nomesCores.add("Preto");
+		nomesCores.add("Azul");
+		nomesCores.add("Roxo");
+		nomesCores.add("Amarelo");
+		nomesCores.add("Rosa");
+		nomesCores.add("Verde");
+		nomesCores.add("Branco");
+
+		return nomesCores;
+
 	}
-	
+
 	public static Integer exercicio2() {
-		
-			List<String> nomes = new ArrayList<String>();
-			nomes.add("Fusca");
-			nomes.add("Mariah");
-		
-			return nomes.size();
+
+		List<String> nomes = new ArrayList<String>();
+		nomes.add("Fusca");
+		nomes.add("Mariah");
+
+		return nomes.size();
 	}
-	
+
 	public static List<String> exercicio3() {
-		
-			List<String> parentes = new ArrayList<String>();
-			parentes.add("pai");
-			parentes.add("mãe");
-			parentes.add("irmãos");
-			parentes.add("irmãs");
-			parentes.add("tios");
-			parentes.add("tias");
-			parentes.add("primos");
-			
-			System.out.println(parentes);
-			
-			parentes.remove("pai");
-			parentes.remove("mãe");
-			
-			System.out.println(parentes);
-			
-			return parentes;
-		
+
+		List<String> parentes = new ArrayList<String>();
+		parentes.add("pai");
+		parentes.add("mãe");
+		parentes.add("irmãos");
+		parentes.add("irmãs");
+		parentes.add("tios");
+		parentes.add("tias");
+		parentes.add("primos");
+
+		System.out.println(parentes);
+
+		parentes.remove("pai");
+		parentes.remove("mãe");
+
+		System.out.println(parentes);
+
+		return parentes;
+
 	}
-	
+
 	public static List<String> exercicio4() {
-		
+
 		List<String> cidades = new ArrayList<String>();
 		cidades.add("Curitiba");
 		cidades.add("Maringá");
 		cidades.add("Cianorte");
-		
+
 		cidades.remove("Maringá");
-		
+
 		System.out.println(cidades);
-		
+
 		return cidades;
+
+	}
+
+	public static List<String> exercicio5() {
+
+		List<String> nomesCores = new ArrayList<String>();
+		nomesCores = exercicio1();
+		Collections.sort(nomesCores);
+
+		System.out.println(nomesCores);
+
+		return nomesCores;
+
+	}
+
+	public static List<String> exercicio6() {
+
+		List<String> nomesCores = new ArrayList<String>();
+		nomesCores = exercicio1();
+		nomesCores.remove("Vermelho");
+
+		System.out.println(nomesCores);
+
+		return nomesCores;
+
+	}
+
+	public static List<String> exercicio7() {
+
+		List<String> parentes = new ArrayList<String>();
+		parentes = exercicio3();
+		Collections.sort(parentes);
+		Collections.reverse(parentes);
+
+		System.out.println(parentes);
+
+		return parentes;
+
+	}
+
+	public static List<Integer> exercicio8() {
+		List<Integer> pares = new ArrayList<Integer>();
+		List<Integer> impares = new ArrayList<Integer>();
+		List<Integer> total = new ArrayList<Integer>();
+		for (Integer posicao = 1; posicao <= 20; posicao++) {
+			if (posicao % 2 == 0) {
+				pares.add(posicao);
+			}
+			if (posicao % 2 != 0) {
+				impares.add(posicao);
+			}
+		}
+		total.addAll(pares);
+		total.addAll(impares);
 		
+		System.out.println(pares);
+		System.out.println(impares);
+		
+		return total;
 	}
 	
+	public static Set<String> exercicio14(){
+		Set<String> nomes = new TreeSet<String>();
+		nomes.add("ANA");
+		nomes.add("ANA LAURA");
+		nomes.add("JOSE");
+		nomes.add("WAGNER");
+		nomes.add("RODOLFO");
+		nomes.add("ROBERVAL");
+		nomes.add("RODOLPHO");
+		nomes.add("VAGNER");
+		nomes.add("JOSÉ");
+		nomes.add("JOALDO");
+		nomes.add("CLECIO");
+		nomes.add("JOSÉ");
+		nomes.add("MARIA");
+		nomes.add("MARCOS");
+		
+		nomes.remove("JOSÉ");
+		
+		System.out.println(nomes);
+		
+		return nomes;	
+	}
+
 }
--- src/test/java/ExercicioColecao/ColecaoSolutionTest.java
+++ src/test/java/ExercicioColecao/ColecaoSolutionTest.java
@@ -5,44 +5,84 @@ import org.junit.Test;
 import br.com.db1.ExercicioDeColecao;
 import static org.junit.Assert.*;
 
-
 public class ColecaoSolutionTest {
 
 	@Test
 	public void exercicio1Test() {
-		
+
 		ColecaoSolution ex1 = new ColecaoSolution();
 		ex1.exercicio1();
 		assertTrue(ex1.exercicio1().get(0).equals("Vermelho"));
 		assertTrue(ex1.exercicio1().get(1).equals("Preto"));
 		assertTrue(ex1.exercicio1().get(2).equals("Azul"));
-		
+
 	}
-	
+
+	@Test
 	public void exercicio2Test() {
-		
+
 		ExercicioDeColecao ex2 = new ExercicioDeColecao();
 		ex2.exercicio2();
-		assertTrue(ex2.exercicio2()== 2);
+		assertTrue(ex2.exercicio2() == 2);
 		assertEquals(2, ex2.exercicio2(), 0);
-		
+
 	}
-	
-	public void exercicio3Test(){
-		
+
+	@Test
+	public void exercicio3Test() {
+
 		ColecaoSolution ex3 = new ColecaoSolution();
 		ex3.exercicio3();
 		assertTrue(ex3.exercicio3().get(0) != "pai");
-		
+
 	}
-	
+
+	@Test
 	public void exercicio4Test() {
-		
+
 		ColecaoSolution ex4 = new ColecaoSolution();
 		ex4.exercicio4();
 		assertTrue(ex4.exercicio4().get(1) != "Maringá");
+
+	}
+
+	@Test
+	public void exercicio5Test() {
+
+		ColecaoSolution ex = new ColecaoSolution();
+		assertTrue(ex.exercicio5().get(0) != "Vermelho");
+
+	}
+
+	@Test
+	public void exercicio6Test() {
+
+		ColecaoSolution ex = new ColecaoSolution();
+		assertTrue(ex.exercicio6().get(0) != "Vermelho");
+
+	}
+
+	@Test
+	public void exercicio7Test() {
+
+		ColecaoSolution ex = new ColecaoSolution();
+		assertTrue(ex.exercicio7().get(0) == "tios");
+
+	}
+
+	@Test
+	public void exercicio8Test() {
+		
+		ColecaoSolution ex = new ColecaoSolution();
+		assertTrue(ex.exercicio8().get(0) == 2);
+	}
+	
+	@Test
+	public void exercicio14Test() {
+		
+		ColecaoSolution ex = new ColecaoSolution();
+		assertTrue(ex.exercicio14().contains("ANA"));
 		
 	}
+	
 }
-
-
